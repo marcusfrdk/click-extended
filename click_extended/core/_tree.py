03facdbcaec4c3979ff7168b206a742d15de55d8
Marcus Fredriksson <marcus@marcusfredriksson.com>
2025-11-10 21:50:16 +0100

    feat: Tree class now works in bottom-to-top registration order to support the decorator bottom-to-top calling pattern

--- click_extended/core/_tree.py
+++ click_extended/core/_tree.py
@@ -1,7 +1,7 @@
 """Singleton class for storing the nodes of the current context."""
 
 from threading import Lock
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Literal, cast
 
 from click_extended.errors.no_parent_error import NoParentError
 from click_extended.errors.no_root_error import NoRootError
@@ -15,21 +15,34 @@ if TYPE_CHECKING:
 
 
 class Tree:
-    """Singleton class for storing the nodes of the current context."""
+    """
+    Singleton class for storing the nodes of the current context.
+
+    This tree is designed to work with decorators, which are applied
+    bottom-to-top. Registrations are queued and then built in reverse
+    order to maintain the hierarchy.
+    """
 
     _instance: "Tree | None" = None
     _lock = Lock()
 
     root: "RootNode | None"
     recent: "ParentNode | None"
+    queue: list[
+        tuple[
+            Literal["root", "parent", "child"],
+            "RootNode | ParentNode | ChildNode",
+        ]
+    ]
 
-    def __new__(cls, *args: Any, **kwargs: Any):
+    def __new__(cls, *args: Any, **kwargs: Any) -> "Tree":
         if cls._instance is None:
             with cls._lock:
                 if cls._instance is None:
                     cls._instance = super().__new__(cls)
                     cls._instance.root = None
                     cls._instance.recent = None
+                    cls._instance.queue = []
         return cls._instance
 
     def __init__(self) -> None:
@@ -44,65 +57,72 @@ class Tree:
     def __reduce__(self) -> tuple[type["Tree"], tuple[()]]:
         return (self.__class__, ())
 
-    def register_root(self, node: RootNode) -> None:
+    def register_root(self, node: "RootNode") -> None:
         """
-        Register the root node.
+        Queue the root node for registration and build the tree.
+
+        In decorator systems, the root is called last, so we can
+        build the tree when it's registered.
 
         Args:
             node (RootNode):
                 The node to register as the root node for the tree.
-
-        Raises:
-            RootNodeExistsError:
-                If a root node already exists.
         """
-        if isinstance(self.root, RootNode):
-            raise RootNodeExistsError
+        self.queue.append(("root", node))
 
-        self.root = node
+        for node_type, node_ins in reversed(self.queue):
+            if node_type == "root":
+                if self.root is not None:
+                    raise RootNodeExistsError
 
-    def register_parent(self, node: ParentNode) -> None:
+                self.root = cast("RootNode", node_ins)
+            elif node_type == "parent":
+                if self.root is None:
+                    raise NoRootError
+
+                if self.root.get(node_ins.name) is not None:
+                    raise ParentNodeExistsError(node_ins.name)
+
+                self.recent = cast("ParentNode", node_ins)
+                self.root[node_ins.name] = node_ins
+            elif node_type == "child":
+                if self.root is None:
+                    raise NoRootError
+
+                if self.recent is None:
+                    raise NoParentError
+
+                name = self.recent.name
+                index = len(self.root[name])
+                self.root[name].children[index] = cast("ChildNode", node_ins)
+
+        self.queue.clear()
+
+    def register_parent(self, node: "ParentNode") -> None:
         """
-        Register a new parent to the tree.
+        Queue a parent node for registration.
+
+        In decorator systems, parents are called before their root but
+        need to be built after the root exists.
 
         Args:
             node (ParentNode):
                 The parent node to register to the tree.
-
-        Raises:
-            ParentNodeExistsError:
-                If a parent already exists with the name.
         """
-        if self.root is None:
-            raise NoRootError
+        self.queue.append(("parent", node))
 
-        if self.root.get(node.name) is not None:
-            raise ParentNodeExistsError(node.name)
-
-        self.recent = node
-        self.root[node.name] = node
-
-    def register_child(self, node: ChildNode) -> None:
+    def register_child(self, node: "ChildNode") -> None:
         """
-        Register a new child to the most recently added parent.
+        Queue a child node for registration.
+
+        In decorator systems, children are called before their parent but
+        need to be built after the parent exists.
 
         Args:
             node (ChildNode):
                 The `ChildNode` to add to the `ParentNode` instance.
-
-        Raises:
-            ParentNotFoundError:
-                If the parent with the name `name` does not exist.
         """
-        if self.root is None:
-            raise NoRootError
-
-        if self.recent is None:
-            raise NoParentError
-
-        name = self.recent.name
-        index = len(self.root[name])
-        self.root[name].children[index] = node
+        self.queue.append(("child", node))
 
     def visualize(self) -> None:
         """Visualize the tree."""
