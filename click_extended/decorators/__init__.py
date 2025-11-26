cc35339a362d7a5d3dd57ca980418f9276076466
Marcus Fredriksson <marcus@marcusfredriksson.com>
2025-11-26 11:16:19 +0100

    feat: Added random_datetime parent decorator

--- click_extended/decorators/__init__.py
+++ click_extended/decorators/__init__.py
@@ -2,6 +2,7 @@
 
 from click_extended.decorators.random_bool import random_bool
 from click_extended.decorators.random_choice import random_choice
+from click_extended.decorators.random_datetime import random_datetime
 from click_extended.decorators.random_float import random_float
 from click_extended.decorators.random_integer import random_integer
 from click_extended.decorators.random_string import random_string
@@ -9,6 +10,7 @@ from click_extended.decorators.random_string import random_string
 __all__ = [
     "random_bool",
     "random_choice",
+    "random_datetime",
     "random_float",
     "random_integer",
     "random_string",
