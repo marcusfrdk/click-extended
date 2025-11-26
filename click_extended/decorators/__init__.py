b2a21f7fb45f85bd89c70d622cbb52cc8922fd25
Marcus Fredriksson <marcus@marcusfredriksson.com>
2025-11-26 09:52:23 +0100

    feat: Added random_integer parent decorator

--- click_extended/decorators/__init__.py
+++ click_extended/decorators/__init__.py
@@ -1,7 +1,9 @@
 """Initialization file for the `click_extended.decorators` module."""
 
+from click_extended.decorators.random_integer import random_integer
 from click_extended.decorators.random_string import random_string
 
 __all__ = [
+    "random_integer",
     "random_string",
 ]
---
+++ click_extended/decorators/random_integer.py
@@ -0,0 +1,43 @@
+"""Parent node for generating a random string."""
+
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-positional-arguments
+
+import random
+from typing import Any
+
+from click_extended.core.context import Context
+from click_extended.core.parent_node import ParentNode
+from click_extended.types import Decorator
+
+
+class RandomInteger(ParentNode):
+    """Parent node for generating random integers."""
+
+    def load(self, context: Context, *args: Any, **kwargs: Any) -> int:
+        if kwargs["max_value"] < kwargs["min_value"]:
+            raise ValueError("min_value can not be larger than max_value.")
+        return random.randint(kwargs["min_value"], kwargs["max_value"])
+
+
+def random_integer(
+    name: str,
+    min_value: int = 0,
+    max_value: int = 100,
+) -> Decorator:
+    """
+    Parent node to generate a random integers.
+
+    Args:
+        name (str):
+            The name of the parent node.
+        min_value (int):
+            The lower value in the range. Defaults to 0.
+        max_value (int):
+            The upper value in the range. Defaults to 100.
+    """
+    return RandomInteger.as_decorator(
+        name=name,
+        min_value=min_value,
+        max_value=max_value,
+    )
