80397c43ee1dc166756a56eb408a9012c88b7348
Marcus Fredriksson <marcus@marcusfredriksson.com>
2025-11-27 11:40:23 +0100

    feat: Improved prompt decorator by refactor and added 'allow_empty' parameter

--- click_extended/core/prompt.py
+++ click_extended/core/prompt.py
@@ -11,21 +11,38 @@ from click_extended.types import Decorator
 class Prompt(ParentNode):
     """Parent decorator that prompts the user for a value."""
 
+    def get_input(self, text: str, hide: bool) -> str:
+        """
+        Get input from the user.
+
+        Args:
+            text (str):
+                The prompt.
+            hide (bool):
+                Whether to hide the user input.
+
+        Returns:
+            str:
+                The value provided from the user.
+        """
+        if hide:
+            return getpass(text)
+        return input(text)
+
     def load(
         self,
         context: Context,
         *args: Any,
         **kwargs: Any,
     ) -> str:
-        if kwargs["hide"]:
-            return getpass(kwargs["text"])
-        return input(kwargs["text"])
+        while True:
+            answer = self.get_input(kwargs["text"], kwargs["hide"])
+            if answer or kwargs["allow_empty"]:
+                return answer
 
 
 def prompt(
-    name: str,
-    text: str = "",
-    hide: bool = False,
+    name: str, text: str = "", hide: bool = False, allow_empty: bool = False
 ) -> Decorator:
     """
     Prompt the user for input.
@@ -37,7 +54,8 @@ def prompt(
             The text to show.
         hide (bool):
             Whether to hide the input, defaults to `False`.
-
+        allow_empty (bool):
+            Whether to allow the input to be empty, defaults to `False`.
 
     Returns:
         Decorator:
@@ -47,4 +65,5 @@ def prompt(
         name=name,
         text=text,
         hide=hide,
+        allow_empty=allow_empty,
     )
